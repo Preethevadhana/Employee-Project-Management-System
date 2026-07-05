83c5269edbb31e9cd0afa3fecf05b4a2510053ac
Preethevadhana <pikkybikky1@gmail.com>
2026-07-05 17:05:00 +0530

    Update models.py
    Add User model

--- models.py
+++ models.py
@@ -1,3 +1,13 @@
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    role = Column(String(20), nullable=False)
