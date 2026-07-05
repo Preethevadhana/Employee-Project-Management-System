c594de72acf61f4655511163c32d04789307ef59
Preethevadhana <pikkybikky1@gmail.com>
2026-07-05 17:10:17 +0530

    Create modules/auth.py
    6

---
+++ modules/auth.py
@@ -0,0 +1,13 @@
+import bcrypt
+
+def hash_password(password):
+    """
+    Hash a plain text password.
+    """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+def verify_password(password, hashed_password):
+    """
+    Verify a plain text password against a hashed password.
+    """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
