6bc3edd718f441048912cb73b6aa147837d771e2
Preethevadhana <pikkybikky1@gmail.com>
2026-07-05 17:07:20 +0530

    Update models.py
    Add Employee model

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = "users"
@@ -11,3 +12,22 @@ class User(Base):
     email = Column(String(100), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
     role = Column(String(20), nullable=False)
+
+
+class Department(Base):
+    __tablename__ = "departments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    department_name = Column(String(100), unique=True, nullable=False)
+    department_head = Column(String(100), nullable=False)
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True, index=True)
+    employee_name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    phone = Column(String(15))
+    designation = Column(String(100))
+    department_id = Column(Integer, ForeignKey("departments.id"))
